792ffac37e13e7885b529d0f4a43432f6a8aa3ec
suntao <suntao.hn@gmail.com>
2026-02-10 22:01:58 +0800

    update vis

--- refmotion_manager/tests/test_loader_cfg.py
+++ refmotion_manager/tests/test_loader_cfg.py
@@ -1,148 +1,273 @@
-from refmotion_manager.motion_loader import RefMotionLoader
 # load dataset for demonstration:
 import glob
 import os
-from refmotion_manager.motion_loader import RefMotionLoader
+
 from refmotion_manager.motion_loader import RefMotionCfg
 
-
-############## Demo trajectory ###################
-num_envs=1
+# Demo trajectory ###################
+num_envs = 1
 using_21_joint = True
 
 # 27 joints
 if not using_21_joint:
-    motion_files=glob.glob(os.getenv("HOME")+"/workspace/lumos_ws/humanoid_demo_retarget/data/lus2_joint27/pkl/Append_Select_S29_*")
-    motion_files=glob.glob(os.getenv("HOME")+"/workspace/lumos_ws/humanoid_demo_retarget/sources/data/motions/lus2_joint27/pkl/CMU_CMU_12_12_04_poses*")
+    motion_files = glob.glob(
+        os.getenv("HOME") + "/workspace/lumos_ws/humanoid_demo_retarget/data/lus2_joint27/pkl/Append_Select_S29_*"
+    )
+    motion_files = glob.glob(
+        os.getenv("HOME")
+        + "/workspace/lumos_ws/humanoid_demo_retarget/sources/data/motions/lus2_joint27/pkl/CMU_CMU_12_12_04_poses*"
+    )
 else:
     # 21 joints
-    #motion_files=glob.glob(os.getenv("HOME")+"/workspace/lumos_ws/humanoid_demo_retarget/sources/data/motions/lus2_joint21/pkl/dance1_subject2.pkl")
-    motion_files=glob.glob(os.getenv("HOME")+"/workspace/lumos_ws/humanoid_demo_retarget/sources/data/motions/lus2_joint21/pkl/fallAndGetUp1_subject1.pkl")
+    # motion_files=glob.glob(os.getenv("HOME")+"/workspace/lumos_ws/humanoid_demo_retarget/sources/data/motions/lus2_joint21/pkl/dance1_subject2.pkl")
+    motion_files = glob.glob(
+        os.getenv("HOME")
+        + "/workspace/lumos_ws/humanoid_demo_retarget/sources/data/motions/lus2_joint21/pkl/fallAndGetUp1_subject1.pkl"
+    )
 
-#motion_files=glob.glob(os.getenv("HOME")+"/workspace/lumos_ws/humanoid_demo_retarget/data/lus2_joint27/pkl/*")
-random_start=False
-amp_obs_frame_num = 2 # minimal is 1, no history amp obs
+# motion_files=glob.glob(os.getenv("HOME")+"/workspace/lumos_ws/humanoid_demo_retarget/data/lus2_joint27/pkl/*")
+random_start = False
+amp_obs_frame_num = 2  # minimal is 1, no history amp obs
 
-############## Init states (carefully)  ###################
+# Init states (carefully)  ###################
 # constants
 INIT_ROOT_STATE_FIELDS = [
-            "root_pos_x",
-            "root_pos_y",
-            "root_pos_z",
-            "root_rot_w",
-            "root_rot_x",
-            "root_rot_y",
-            "root_rot_z",
-            "root_vel_x_b",
-            "root_vel_y_b",
-            "root_vel_z_b",
-            "root_ang_vel_x_b",
-            "root_ang_vel_y_b",
-            "root_ang_vel_z_b",
-        ]
+    "root_pos_x",
+    "root_pos_y",
+    "root_pos_z",
+    "root_rot_w",
+    "root_rot_x",
+    "root_rot_y",
+    "root_rot_z",
+    "root_vel_x_b",
+    "root_vel_y_b",
+    "root_vel_z_b",
+    "root_ang_vel_x_b",
+    "root_ang_vel_y_b",
+    "root_ang_vel_z_b",
+]
 
 # 27 joints
-lus2_27joint_names = ['left_hip_pitch_joint', 'right_hip_pitch_joint', 'torso_joint', 'left_hip_roll_joint', 'right_hip_roll_joint', 'left_shoulder_pitch_joint', 'right_shoulder_pitch_joint', 'left_hip_yaw_joint', 'right_hip_yaw_joint', 'left_shoulder_roll_joint', 'right_shoulder_roll_joint', 'left_knee_joint', 'right_knee_joint', 'left_shoulder_yaw_joint', 'right_shoulder_yaw_joint', 'left_ankle_pitch_joint', 'right_ankle_pitch_joint', 'left_elbow_joint', 'right_elbow_joint', 'left_ankle_roll_joint', 'right_ankle_roll_joint', 'left_wrist_yaw_joint',                    'right_wrist_yaw_joint', 'left_wrist_pitch_joint', 'right_wrist_pitch_joint',
-'left_wrist_roll_joint', 'right_wrist_roll_joint'
+lus2_27joint_names = [
+    "left_hip_pitch_joint",
+    "right_hip_pitch_joint",
+    "torso_joint",
+    "left_hip_roll_joint",
+    "right_hip_roll_joint",
+    "left_shoulder_pitch_joint",
+    "right_shoulder_pitch_joint",
+    "left_hip_yaw_joint",
+    "right_hip_yaw_joint",
+    "left_shoulder_roll_joint",
+    "right_shoulder_roll_joint",
+    "left_knee_joint",
+    "right_knee_joint",
+    "left_shoulder_yaw_joint",
+    "right_shoulder_yaw_joint",
+    "left_ankle_pitch_joint",
+    "right_ankle_pitch_joint",
+    "left_elbow_joint",
+    "right_elbow_joint",
+    "left_ankle_roll_joint",
+    "right_ankle_roll_joint",
+    "left_wrist_yaw_joint",
+    "right_wrist_yaw_joint",
+    "left_wrist_pitch_joint",
+    "right_wrist_pitch_joint",
+    "left_wrist_roll_joint",
+    "right_wrist_roll_joint",
 ]
 
 # 21 joints
-lus2_21joint_names = ['left_hip_pitch_joint', 'right_hip_pitch_joint', 'torso_joint', 'left_hip_roll_joint', 'right_hip_roll_joint', 'left_shoulder_pitch_joint', 'right_shoulder_pitch_joint', 'left_hip_yaw_joint', 'right_hip_yaw_joint', 'left_shoulder_roll_joint', 'right_shoulder_roll_joint', 'left_knee_joint', 'right_knee_joint', 'left_shoulder_yaw_joint', 'right_shoulder_yaw_joint', 'left_ankle_pitch_joint', 'right_ankle_pitch_joint', 'left_elbow_joint', 'right_elbow_joint', 'left_ankle_roll_joint', 'right_ankle_roll_joint'
+lus2_21joint_names = [
+    "left_hip_pitch_joint",
+    "right_hip_pitch_joint",
+    "torso_joint",
+    "left_hip_roll_joint",
+    "right_hip_roll_joint",
+    "left_shoulder_pitch_joint",
+    "right_shoulder_pitch_joint",
+    "left_hip_yaw_joint",
+    "right_hip_yaw_joint",
+    "left_shoulder_roll_joint",
+    "right_shoulder_roll_joint",
+    "left_knee_joint",
+    "right_knee_joint",
+    "left_shoulder_yaw_joint",
+    "right_shoulder_yaw_joint",
+    "left_ankle_pitch_joint",
+    "right_ankle_pitch_joint",
+    "left_elbow_joint",
+    "right_elbow_joint",
+    "left_ankle_roll_joint",
+    "right_ankle_roll_joint",
 ]
 
 all_joint_names = lus2_21joint_names if using_21_joint else lus2_27joint_names
 
 # NOTE, this should follow the order of that in env when loading usd model
-INIT_STATE_FIELDS = INIT_ROOT_STATE_FIELDS + [key+"_dof_pos" for key in all_joint_names] + [key+"_dof_vel" for key in all_joint_names]
-
-
-############## Style  (AMP) ########################
-# not need to include that for normal command (base_velocity command)
-
-style_root_fields = [
-        #"root_pos_z",
-        "root_rot_w",
-        "root_rot_x",
-        "root_rot_y",
-        "root_rot_z",
-        #"root_vel_x_b",
-        #"root_vel_y_b",
-        #"root_vel_z_b",
-        #"root_ang_vel_x_b",
-        #"root_ang_vel_y_b",
-        #"root_ang_vel_z_b",
-        ]
-
-# 27 joints
-style_joint_name = [
-        'left_hip_roll_joint', 'right_hip_roll_joint',  'left_hip_yaw_joint', 'right_hip_yaw_joint',
-        'left_hip_pitch_joint', 'right_hip_pitch_joint',
-        'left_knee_joint', 'right_knee_joint',
-        'torso_joint',
-        'left_shoulder_pitch_joint', 'right_shoulder_pitch_joint',
-        'left_shoulder_roll_joint', 'right_shoulder_roll_joint',
-        'left_shoulder_yaw_joint', 'right_shoulder_yaw_joint',
-        'left_elbow_joint', 'right_elbow_joint'] + [] if using_21_joint else ['left_wrist_yaw_joint', 'right_wrist_yaw_joint', 'left_wrist_pitch_joint', 'right_wrist_pitch_joint', 'left_wrist_roll_joint', 'right_wrist_roll_joint']
-
-
-style_fields = style_root_fields + \
-        ([key + "_dof_pos" for key in style_joint_name] if style_joint_name is not None else [])
-        #([key + "_dof_vel" for key in style_joint_name] if style_joint_name is not None else []) + \
-        #([k1 + k2 for k1 in style_body_name for k2 in ["_pos_x_b", "_pos_y_b", "_pos_z_b"]] if style_body_name is not None else [])
-
-
-# carefully to change this
-style_goal_fields=[
-            "root_rot_w",
-            "root_rot_x",
-            "root_rot_y",
-            "root_rot_z",
-        ]
-
-style_goal_fields = None
-track_style_goal_weight =  1.47
-style_reward_coef = 1.0
-
-##############  Expressive (Mimic) ########################
-
-#link_name =  ["right_knee_link", "left_elbow_link"]
-expressive_link_name = ["left_elbow_link", "right_elbow_link","left_hip_pitch_link","right_hip_pitch_link","left_shoulder_roll_link","right_shoulder_roll_link","left_ankle_roll_link","right_ankle_roll_link", "left_knee_link", "right_knee_link","left_wrist_roll_link","right_wrist_roll_link"]
-expressive_link_name =  None #["left_elbow_link", "left_hip_pitch_link"] #,"right_hip_pitch_link","left_ankle_roll_link","right_ankle_roll_link", "left_knee_link", "right_knee_link"]
-
-# 27 joints robot
-upper_joint_name = ["torso_joint", "left_shoulder_pitch_joint", "right_shoulder_pitch_joint","left_shoulder_roll_joint","right_shoulder_roll_joint",  "left_shoulder_yaw_joint","right_shoulder_yaw_joint","left_elbow_joint", "right_elbow_joint"] + [] if using_21_joint else ["left_wrist_yaw_joint","right_wrist_yaw_joint", 'left_wrist_roll_joint', 'right_wrist_roll_joint']
-
-lower_joint_name = ["left_hip_pitch_joint", "right_hip_pitch_joint", "left_hip_yaw_joint", "right_hip_yaw_joint", "left_hip_roll_joint", "right_hip_roll_joint", "left_knee_joint","right_knee_joint"]
-feet_joint_name = ["left_ankle_pitch_joint", "right_ankle_pitch_joint","left_ankle_roll_joint","right_ankle_roll_joint"]
-joint_name = upper_joint_name + lower_joint_name + feet_joint_name
-
-# not need to include that for normal command (base_velocity command)
-expressive_goal_fields = [key+"_dof_pos" for key in joint_name] if joint_name is not None else None
-#expressive_goal_fields += [key+"_dof_vel" for key in joint_name] if joint_name is not None else []
-expressive_goal_fields += [key1+key2 for key1 in expressive_link_name for key2 in ["_pos_x_b","_pos_y_b","_pos_z_b"]] if expressive_link_name is not None else []
-expressive_goal_fields += [key1+key2 for key1 in expressive_link_name for key2 in ["_vel_x_b","_vel_y_b","_vel_z_b"]] if expressive_link_name is not None else []
-
-episode_length_s = 10
-###################### amp data #################
-ref_motion_cfg = RefMotionCfg(
-        motion_files=motion_files,
-        init_state_fields=INIT_STATE_FIELDS,
-        style_goal_fields=style_goal_fields, # as input for the policy
-        style_fields=style_fields, # as for style rewards
-        expressive_goal_fields = expressive_goal_fields, # only as input for the policy
-        #expressive_joint_name = joint_name, # for tracking rewards
-        #expressive_link_name = expressive_link_name, # for tracking rewards
-        time_between_frames=0.02,  # time between two frames of state and next_state
-        shuffle=False, #shuffle different trajectories
-        random_start=random_start,
-        amp_obs_frame_num=amp_obs_frame_num, #-1+1,
-        ref_length_s=episode_length_s, # 20 s
-        clip_num=num_envs,
-        frame_begin=None,
-        frame_end=None,
+INIT_STATE_FIELDS = (
+    INIT_ROOT_STATE_FIELDS
+    + [key + "_dof_pos" for key in all_joint_names]
+    + [key + "_dof_vel" for key in all_joint_names]
 )
 
 
+# Style  (AMP) ########################
+# not need to include that for normal command (base_velocity command)
+
+style_root_fields = [
+    # "root_pos_z",
+    "root_rot_w",
+    "root_rot_x",
+    "root_rot_y",
+    "root_rot_z",
+    # "root_vel_x_b",
+    # "root_vel_y_b",
+    # "root_vel_z_b",
+    # "root_ang_vel_x_b",
+    # "root_ang_vel_y_b",
+    # "root_ang_vel_z_b",
+]
+
+# 27 joints
+style_joint_name = (
+    [
+        "left_hip_roll_joint",
+        "right_hip_roll_joint",
+        "left_hip_yaw_joint",
+        "right_hip_yaw_joint",
+        "left_hip_pitch_joint",
+        "right_hip_pitch_joint",
+        "left_knee_joint",
+        "right_knee_joint",
+        "torso_joint",
+        "left_shoulder_pitch_joint",
+        "right_shoulder_pitch_joint",
+        "left_shoulder_roll_joint",
+        "right_shoulder_roll_joint",
+        "left_shoulder_yaw_joint",
+        "right_shoulder_yaw_joint",
+        "left_elbow_joint",
+        "right_elbow_joint",
+    ]
+    + []
+    if using_21_joint
+    else [
+        "left_wrist_yaw_joint",
+        "right_wrist_yaw_joint",
+        "left_wrist_pitch_joint",
+        "right_wrist_pitch_joint",
+        "left_wrist_roll_joint",
+        "right_wrist_roll_joint",
+    ]
+)
 
 
+style_fields = style_root_fields + (
+    [key + "_dof_pos" for key in style_joint_name] if style_joint_name is not None else []
+)
+# ([key + "_dof_vel" for key in style_joint_name] if style_joint_name is not None else []) + \
+# ([k1 + k2 for k1 in style_body_name for k2 in ["_pos_x_b", "_pos_y_b", "_pos_z_b"]] if style_body_name is not None else [])
 
+
+# carefully to change this
+style_goal_fields = [
+    "root_rot_w",
+    "root_rot_x",
+    "root_rot_y",
+    "root_rot_z",
+]
+
+style_goal_fields = None
+track_style_goal_weight = 1.47
+style_reward_coef = 1.0
+
+#  Expressive (Mimic) ########################
+
+# link_name =  ["right_knee_link", "left_elbow_link"]
+expressive_link_name = [
+    "left_elbow_link",
+    "right_elbow_link",
+    "left_hip_pitch_link",
+    "right_hip_pitch_link",
+    "left_shoulder_roll_link",
+    "right_shoulder_roll_link",
+    "left_ankle_roll_link",
+    "right_ankle_roll_link",
+    "left_knee_link",
+    "right_knee_link",
+    "left_wrist_roll_link",
+    "right_wrist_roll_link",
+]
+expressive_link_name = None  # ["left_elbow_link", "left_hip_pitch_link"] #,"right_hip_pitch_link","left_ankle_roll_link","right_ankle_roll_link", "left_knee_link", "right_knee_link"]
+
+# 27 joints robot
+upper_joint_name = (
+    [
+        "torso_joint",
+        "left_shoulder_pitch_joint",
+        "right_shoulder_pitch_joint",
+        "left_shoulder_roll_joint",
+        "right_shoulder_roll_joint",
+        "left_shoulder_yaw_joint",
+        "right_shoulder_yaw_joint",
+        "left_elbow_joint",
+        "right_elbow_joint",
+    ]
+    + []
+    if using_21_joint
+    else ["left_wrist_yaw_joint", "right_wrist_yaw_joint", "left_wrist_roll_joint", "right_wrist_roll_joint"]
+)
+
+lower_joint_name = [
+    "left_hip_pitch_joint",
+    "right_hip_pitch_joint",
+    "left_hip_yaw_joint",
+    "right_hip_yaw_joint",
+    "left_hip_roll_joint",
+    "right_hip_roll_joint",
+    "left_knee_joint",
+    "right_knee_joint",
+]
+feet_joint_name = [
+    "left_ankle_pitch_joint",
+    "right_ankle_pitch_joint",
+    "left_ankle_roll_joint",
+    "right_ankle_roll_joint",
+]
+joint_name = upper_joint_name + lower_joint_name + feet_joint_name
+
+# not need to include that for normal command (base_velocity command)
+expressive_goal_fields = [key + "_dof_pos" for key in joint_name] if joint_name is not None else None
+# expressive_goal_fields += [key+"_dof_vel" for key in joint_name] if joint_name is not None else []
+expressive_goal_fields += (
+    [key1 + key2 for key1 in expressive_link_name for key2 in ["_pos_x_b", "_pos_y_b", "_pos_z_b"]]
+    if expressive_link_name is not None
+    else []
+)
+expressive_goal_fields += (
+    [key1 + key2 for key1 in expressive_link_name for key2 in ["_vel_x_b", "_vel_y_b", "_vel_z_b"]]
+    if expressive_link_name is not None
+    else []
+)
+
+episode_length_s = 10
+# amp data #################
+ref_motion_cfg = RefMotionCfg(
+    motion_files=motion_files,
+    init_state_fields=INIT_STATE_FIELDS,
+    style_goal_fields=style_goal_fields,  # as input for the policy
+    style_fields=style_fields,  # as for style rewards
+    expressive_goal_fields=expressive_goal_fields,  # only as input for the policy
+    # expressive_joint_name = joint_name, # for tracking rewards
+    # expressive_link_name = expressive_link_name, # for tracking rewards
+    time_between_frames=0.008,  # time between two frames of state and next_state
+    shuffle=False,  # shuffle different trajectories
+    random_start=random_start,
+    amp_obs_frame_num=amp_obs_frame_num,  # -1+1,
+    ref_length_s=episode_length_s,  # 20 s
+    clip_num=num_envs,
+    frame_begin=None,
+    frame_end=None,
+)
--- refmotion_manager/vis_mj.py
+++ refmotion_manager/vis_mj.py
@@ -1,30 +1,20 @@
-import os
-import glob
-import sys
-import time
-import argparse
-import json
-import os.path as osp
-import scipy.ndimage.filters as filters
-sys.path.append(os.getcwd())
-import torch
-import numpy as np
-import math
-from copy import deepcopy
-from collections import defaultdict
-import mujoco
-import mujoco.viewer
-from scipy.spatial.transform import Rotation as sRot
-import joblib
-import hydra
-from omegaconf import DictConfig, OmegaConf
-from refmotion_manager.motion_loader import RefMotionLoader, RefMotionCfg
-
 # Configure the logging system
 import logging
+import numpy as np
+import os
+import sys
+import time
+
+import hydra
+import mujoco
+import mujoco.viewer
+from omegaconf import DictConfig
+from refmotion_manager.motion_loader import RefMotionLoader
+
+sys.path.append(os.getcwd())
 logging.basicConfig(
     level=logging.INFO,  # Set the minimum logging level
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'  # Define the log message format
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Define the log message format
 )
 # Create a logger object
 logger = logging.getLogger("vis_mj")
@@ -36,97 +26,123 @@ def add_visual_capsule(scene, pos, radius, rgba):
         return
     scene.ngeom += 1  # increment ngeom
 
-    #viewer.user_scn.ngeom = 0
+    # viewer.user_scn.ngeom = 0
     mujoco.mjv_initGeom(
-          scene.geoms[scene.ngeom-1],
-          type=mujoco.mjtGeom.mjGEOM_SPHERE,
-          size=[radius, 0, 0],
-          pos=0.1*np.array(pos),
-          mat=np.eye(3).flatten(),
-          rgba=0.5*np.array(rgba)
-      )
+        scene.geoms[scene.ngeom - 1],
+        type=mujoco.mjtGeom.mjGEOM_SPHERE,
+        size=[radius, 0, 0],
+        pos=0.1 * np.array(pos),
+        mat=np.eye(3).flatten(),
+        rgba=0.5 * np.array(rgba),
+    )
 
 
-
-def key_call_back( keycode):
-    global curr_start, num_motions, motion_id, motion_acc, time_step, dt, paused, motion_data_keys
+def key_call_back(keycode):
+    global paused
     if chr(keycode) == "R":
         logger.info("Reset")
-    #else:
+    elif chr(keycode) == " ":
+        logger.info("Paused")
+        paused = not paused
+    # else:
     #    logger.info("not mapped", chr(keycode))
-    
-    
+
 
 @hydra.main(version_base=None, config_path="./cfg", config_name="config")
-def main(cfg : DictConfig) -> None:
-    #1) loading ref traj by refmotion_manager
+def main(cfg: DictConfig) -> None:
+    global paused
+    paused = False
+    # 1) loading ref traj by refmotion_manager
     # load dataset for demonstration
     from refmotion_manager.tests.test_loader_cfg import ref_motion_cfg
+
     ref_motion_cfg.time_between_frames = 0.008
 
-    dataset = cfg.get("dataset",None)
-    motion_files = "./"+os.path.join(dataset.folder, dataset.file)
+    dataset = cfg.get("dataset")
+    motion_files = "./" + os.path.join(dataset.folder, dataset.file)
     logger.info(f"motion file is {motion_files}")
-    ref_motion_cfg.motion_files= motion_files
-    ref_motion_cfg.device="cpu"
-    ref_motion_cfg.ref_length_s=None
+    ref_motion_cfg.motion_files = motion_files
+    ref_motion_cfg.device = "cpu"
+    ref_motion_cfg.ref_length_s = None
     ref_motion_cfg.clip_num = 1
     ref_motion = RefMotionLoader(ref_motion_cfg)
-    
 
-    #2) build robot model
-    asset = cfg.get("asset",None)
+    # 2) build robot model
+    asset = cfg.get("asset")
     humanoid_xml = os.path.join(asset.root, asset.xml)
     logger.info(f"humanoid xlm is {humanoid_xml}")
     mj_model = mujoco.MjModel.from_xml_path(humanoid_xml)
     mj_data = mujoco.MjData(mj_model)
     joint_names = [mj_model.joint(i).name for i in range(1, mj_model.njnt)]
 
-    #3) cfg 
+    # 3) cfg
     dt = ref_motion_cfg.time_between_frames
-    root_fields= ["root_pos_x","root_pos_y", "root_pos_z","root_rot_w","root_rot_x","root_rot_y","root_rot_z"]
-    joint_dof_fields = [key+"_dof_pos" for key in joint_names]
+    root_fields = ["root_pos_x", "root_pos_y", "root_pos_z", "root_rot_w", "root_rot_x", "root_rot_y", "root_rot_z"]
+    joint_dof_fields = [key + "_dof_pos" for key in joint_names]
     data_fields = root_fields + joint_dof_fields
     print(f" data fields : {data_fields}")
     index = [ref_motion.trajectory_fields.index(key) for key in data_fields]
 
-    robot_bodies = ["pelvis","left_elbow_link", "right_elbow_link","left_hip_pitch_link","right_hip_pitch_link","left_shoulder_roll_link","right_shoulder_roll_link","left_ankle_roll_link","right_ankle_roll_link", "left_knee_link", "right_knee_link"] #"left_wrist_roll_link","right_wrist_roll_link"]
-    body_index = [[ref_motion.trajectory_fields.index(key + kk) for kk in ["_pos_x_w", "_pos_y_w", "_pos_z_w"]] for key in robot_bodies]
+    robot_bodies = [
+        "pelvis",
+        "left_elbow_link",
+        "right_elbow_link",
+        "left_hip_pitch_link",
+        "right_hip_pitch_link",
+        "left_shoulder_roll_link",
+        "right_shoulder_roll_link",
+        "left_ankle_roll_link",
+        "right_ankle_roll_link",
+        "left_knee_link",
+        "right_knee_link",
+    ]  # "left_wrist_roll_link","right_wrist_roll_link"]
+    body_index = [
+        [ref_motion.trajectory_fields.index(key + kk) for kk in ["_pos_x_w", "_pos_y_w", "_pos_z_w"]]
+        for key in robot_bodies
+    ]
     print(f" body index : {body_index}")
 
-    with mujoco.viewer.launch_passive(mj_model, mj_data, key_callback=key_call_back, show_left_ui=False, show_right_ui=False) as viewer:
+    with mujoco.viewer.launch_passive(
+        mj_model, mj_data, key_callback=key_call_back, show_left_ui=False, show_right_ui=False
+    ) as viewer:
         cam = viewer.cam
-        cam.distance = 4.0 ;cam.azimuth = 135; cam.elevation = -10; cam.lookat = [0,0,0]
-        cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING;cam.trackbodyid=1;
+        cam.distance = 4.0
+        cam.azimuth = 135
+        cam.elevation = -10
+        cam.lookat = [0, 0, 0]
+        cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
+        cam.trackbodyid = 1
 
-        # adding gemo to dislay key joint position
+        # adding gemo to display key joint position
         for _ in range(20):
             add_visual_capsule(viewer.user_scn, np.zeros(3), 0.05, np.array([1, 0, 0, 1]))
         for _ in range(20):
-            add_visual_capsule(viewer.user_scn, np.zeros(3), 0.05,  np.array([0, 1, 0, 1]))
+            add_visual_capsule(viewer.user_scn, np.zeros(3), 0.05, np.array([0, 1, 0, 1]))
         # Close the viewer automatically after 30 wall-seconds.
         while viewer.is_running():
             cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
             step_start = time.time()
-            mj_data.qpos[:] = ref_motion.data[:,index].cpu().numpy()
-        
-            mujoco.mj_forward(mj_model, mj_data)
-            ref_motion.step()
+            mj_data.qpos[:] = ref_motion.data[:, index].cpu().numpy()
 
-            if ref_motion.frame_idx > ref_motion.clip_frame_num-4:
+            mujoco.mj_forward(mj_model, mj_data)
+            if not paused:
+                ref_motion.step()
+
+            if ref_motion.frame_idx > ref_motion.clip_frame_num - 4:
                 ref_motion.reset()
-                logger.info(f"Reset")
+                logger.info("Reset")
 
             # visualizing robot joints
             if robot_bodies is not None:
-                for i,idx in enumerate(body_index):
-                    viewer.user_scn.geoms[20+i].pos = ref_motion.data[:, idx]
-            
+                for i, idx in enumerate(body_index):
+                    viewer.user_scn.geoms[20 + i].pos = ref_motion.data[:, idx]
+
             # Pick up changes to the physics state, apply perturbations, update options from GUI.
             viewer.sync()
             time_until_next_step = dt - (time.time() - step_start)
             if time_until_next_step > 0:
                 time.sleep(time_until_next_step)
+
 
 if __name__ == "__main__":
     main()
